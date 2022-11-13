70eb6470327bc53dd4b1f55867717dbe85dc3f40
Olga Bel <olza91@mail.ru>
2022-11-13 21:54:12 +0300

    dz8

---
+++ z2/Program.cs
@@ -0,0 +1,70 @@
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+void NewArray(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      arr[i, j] = new Random().Next(1, 10);
+    }
+  }
+}
+
+void PrintArray(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+int[,] array = new int[5, 4];
+NewArray(array);
+PrintArray(array);
+
+
+int minSumLine = 0;
+int sumLine = SumLineElements(array, 0);
+
+for (int i = 1; i < array.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(array, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+
+
+int SumLineElements(int[,] arr, int i)
+{
+  int sumLine = arr[i,0];
+  for (int j = 1; j < arr.GetLength(1); j++)
+  {
+    sumLine += arr[i,j];
+  }
+  return sumLine;
+}
+
+
+
---
+++ z3/Program.cs
@@ -0,0 +1,69 @@
+﻿/* 
+Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.(посмотрите как реализуется произведение матриц, там не просто перемножение элемент на элемент)
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+
+Console.Clear();
+
+int[,] firstMartrix = new int[2, 2];
+NewArray(firstMartrix);
+Console.WriteLine($"\nПервая матрица:");
+PrintArray(firstMartrix);
+
+int[,] secomdMartrix = new int[2, 2];
+NewArray(secomdMartrix);
+Console.WriteLine($"\nВторая матрица:");
+PrintArray(secomdMartrix);
+
+int[,] resultMatrix = new int[2, 2];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"\nПроизведение первой и второй матриц:");
+PrintArray(resultMatrix);
+
+void MultiplyMatrix(int[,] firsMartrix, int[,] secomMartrix, int[,] resulMatrix)
+{
+  for (int i = 0; i < resulMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resulMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firsMartrix.GetLength(1); k++)
+      {
+        sum += firsMartrix[i,k] * secomMartrix[k,j];
+      }
+      resulMatrix[i,j] = sum;
+    }
+  }
+}
+
+
+
+void NewArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(1, 7);
+    }
+  }
+}
+
+void PrintArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
